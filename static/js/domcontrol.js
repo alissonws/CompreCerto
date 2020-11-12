0137bd085366ce3d677fa9fb61ffe05ec661c811
Alisson W. Corrêa <alissonwcorrea@gmail.com>
2020-11-12 08:47:26 -0300

    comentários da funcionalidades adicionadas no commit passado

--- static/js/domcontrol.js
+++ static/js/domcontrol.js
@@ -1,5 +1,7 @@
 class DOMControl {
+  // Classe responsável por adicionar event listeners e processar inputs
   constructor () {
+    // Objeto que armazena as funções de cada botão
     this.listeners = {
       'add_card': {
         'event': 'click',
@@ -8,12 +10,19 @@ class DOMControl {
     }
   }
 
+  // Função que realiza o bind de cada botão com sua função
   updateListeners () {
+    // Referência da própria classe
     var this_class = this
+    // Lista de elementos
     var ids = Object.keys(this.listeners)
 
+    // Iterando pela lista de elementos e fazendo adicionando o event listener da sua respectiva função
     ids.forEach(function (key) {
-      document.getElementById(key).addEventListener(this_class.listeners[key].event, this_class.listeners[key].method)
+      let event = this_class.listeners[key].event;
+      let method = this_class.listeners[key].method;
+
+      document.getElementById(key).addEventListener(event, method)
     })
   }
 
